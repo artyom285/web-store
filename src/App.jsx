7a625c3a061caf6f1fa8d7890c41b912e00df35c
artyom285 <artyom.shlyahtin285@gmail.com>
2023-08-06 16:50:26 +0800

    lazy commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css';
+import React, { useState } from 'react';
 import { Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar';
 import Home from "./components/pages/Home";
-import ForYou from './components/pages/ForYou';
-import DiscountsPage from './components/pages/DiscountsPage';
-import FreePage from "./components/pages/FreePage"
-import New from './components/pages/New';
-import Trend from './components/pages/Trend';
-import PopularPage from "./components/pages/PopularPage";
-import GamePage from './components/GamePage';
-import { useState } from 'react';
 import useFetch from './components/useFetch';
 import Sidebar from "./components/Sidebar";
+
+const LazyForYou = React.lazy(() => import("./components/pages/ForYou"));
+const LazyDiscountsPage = React.lazy(() => import("./components/pages/DiscountsPage"));
+const LazyFreePage = React.lazy(() => import("./components/pages/FreePage"));
+const LazyNew = React.lazy(() => import("./components/pages/New"));
+const LazyTrend = React.lazy(() => import("./components/pages/Trend"));
+const LazyPopularPage = React.lazy(() => import("./components/pages/PopularPage"));
+const LazyGamePage = React.lazy(() => import("./components/GamePage"));
 
 function App() {
   const [sidebar, setSidebar] = useState(false);
@@ -25,13 +26,13 @@ function App() {
       <Navbar click={sideFunc} />
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/foryou' element={<ForYou foryou={game} />}></Route>
-        <Route path='/discounts' element={<DiscountsPage sales={game} />}></Route>
-        <Route path='/free' element={<FreePage noprice={game} />}></Route>
-        <Route path='/new' element={<New latest={game} />}></Route>
-        <Route path='/trending' element={<Trend fancy={game} />}></Route>
-        <Route path='/popular' element={<PopularPage hype={game} />}></Route>
-        <Route path='/game-page/:id' element={<GamePage />}></Route>
+        <Route path='/foryou' element={<React.Suspense fallback="Loading..."><LazyForYou foryou={game} /></React.Suspense>}></Route>
+        <Route path='/discounts' element={<React.Suspense fallback="Loading..."><LazyDiscountsPage sales={game} /></React.Suspense>}></Route>
+        <Route path='/free' element={<React.Suspense fallback="Loading..."><LazyFreePage noprice={game} /></React.Suspense>}></Route>
+        <Route path='/new' element={<React.Suspense fallback="Loading..."><LazyNew latest={game} /></React.Suspense>}></Route>
+        <Route path='/trending' element={<React.Suspense fallback="Loading..."><LazyTrend fancy={game} /></React.Suspense>}></Route>
+        <Route path='/popular' element={<React.Suspense fallback="Loading..."><LazyPopularPage hype={game} /></React.Suspense>}></Route>
+        <Route path='/game-page/:id' element={<React.Suspense fallback="Loading..."><LazyGamePage /></React.Suspense>}></Route>
       </Routes>
     </div>
   )
