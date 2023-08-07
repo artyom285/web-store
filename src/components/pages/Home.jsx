f712fd0e34648d682ca880e17912af3f500a02e6
artyom285 <artyom.shlyahtin285@gmail.com>
2023-08-07 14:05:46 +0800

    home commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import Banners from '../home/Banners';
-import Discounts from '../home/Discounts';
-import Free from '../home/Free';
-import Fresh from "../home/Fresh";
-import Trending from '../home/Trending';
-import Popular from '../home/Popular';
 import useFetch from '../useFetch';
+
+const LazyBanner = React.lazy(() => import("../home/Banners"));
+const LazyDiscounts = React.lazy(() => import("../home/Discounts"));
+const LazyFree = React.lazy(() => import("../home/Free"));
+const LazyFresh = React.lazy(() => import("../home/Fresh"));
+const LazyTrending = React.lazy(() => import("../home/Trending"));
+const LazyPopular = React.lazy(() => import("../home/Popular"));
 
 function Home() {
   const { game } = useFetch("https://games-api-m8ak.onrender.com/games");
 
   return (
     <div className='Home'>
-        {game && <Banners banners={game} />}
-        {game && <Discounts discounts={game} />}
-        {game && <Free free={game} />}
-        {game && <Fresh fresh={game} />}
-        {game && <Trending trend={game} />}
-        {game && <Popular popular={game} />}
+        {game && <React.Suspense fallback={<div>Loading...</div>}><LazyBanner banners={game} /></React.Suspense>}
+        {game && <React.Suspense fallback={<div>Loading...</div>}><LazyDiscounts discounts={game} /></React.Suspense>}
+        {game && <React.Suspense fallback={<div>Loading...</div>}><LazyFree free={game} /></React.Suspense>}
+        {game && <React.Suspense fallback={<div>Loading...</div>}><LazyFresh fresh={game} /></React.Suspense>}
+        {game && <React.Suspense fallback={<div>Loading...</div>}><LazyTrending trend={game} /></React.Suspense>}
+        {game && <React.Suspense fallback={<div>Loading...</div>}><LazyPopular popular={game} /></React.Suspense>}
     </div>
   )
 }
